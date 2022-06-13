d4ba7be5e1b57578ebf190d713d3ad87b7f0cb54
Elena Khamlyuk <elena.khamlyuk@devexpress.com>
2022-06-13 14:38:50 +0300

    upd

--- CS/SaveAndRestoreLayout/SaveAndRestoreLayout/Data/WeatherForecastService.cs
+++ CS/SaveAndRestoreLayout/SaveAndRestoreLayout/Data/WeatherForecastService.cs
@@ -1,17 +1,11 @@
-namespace SaveRestoreDataGridLayout.Data
-{
-    public class WeatherForecastService
-    {
-        private static readonly string[] Summaries = new[]
-        {
+namespace SaveAndRestoreLayout.Data {
+    public class WeatherForecastService {
+        private static readonly string[] Summaries = new[] {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
-
-        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
-        {
+        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate) {
             var rng = new Random();
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
+            return Task.FromResult(Enumerable.Range(1, 100).Select(index => new WeatherForecast {
                 Date = startDate.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
--- CS/SaveAndRestoreLayout/SaveAndRestoreLayout/Pages/Error.cshtml.cs
+++ CS/SaveAndRestoreLayout/SaveAndRestoreLayout/Pages/Error.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace SaveRestoreDataGridLayout.Pages {
+namespace SaveAndRestoreLayout.Pages {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
     public class ErrorModel : PageModel {
