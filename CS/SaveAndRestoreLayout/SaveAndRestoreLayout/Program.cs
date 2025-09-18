fbcd10bf14f9223fb5ca1c88a4636e28e08bfc79
Elena Peskova <116714417+elenapeskova@users.noreply.github.com>
2025-09-18 12:23:34 +0400

    Change SizeMode to Medium in DevExpress options
    Updated DevExpress Blazor configuration to set SizeMode.

--- CS/SaveAndRestoreLayout/SaveAndRestoreLayout/Program.cs
+++ CS/SaveAndRestoreLayout/SaveAndRestoreLayout/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddDevExpressBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.Configure<DevExpress.Blazor.Configuration.GlobalOptions>(options => {
-    options.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5;
+    options.SizeMode = DevExpress.Blazor.SizeMode.Medium;
 });
 
 var app = builder.Build();
